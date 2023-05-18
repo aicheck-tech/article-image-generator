47e2d9db9fe9267abd3d471d3ca43baeb6c7d653
Jan Soukeník <59582869+Dzeniks@users.noreply.github.com>
2023-05-18 15:57:45 +0200

    Web crawler with Apify SDK for BBC news (#8)
    * Web crawler with Apify SDK for BBC news
    
    * Readme.md update, workflow improvment

---
+++ web_scrapers/BBC/Readme.md
@@ -0,0 +1,19 @@
+## About:
+Web scrapper for BBC news
+
+### Requirements:
+1. [NodeJS 18.16.0 LTS](https://nodejs.org/en)
+
+## Getting Started
+
+1. Get all packages
+
+```cmd
+npm install
+```
+
+2. Run web crawler, which you want
+
+```cmd
+node bbc_news_cheerio_crawler.js
+```
---
+++ web_scrapers/BBC/extract_storage.py
@@ -0,0 +1,37 @@
+import json
+from pathlib import Path
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+DIRECTORY_PATH = Path(__file__).parent / "storage/datasets/default/"
+OUTPUT_PATH = Path(__file__).parent / "merged.json"
+
+
+def merge_json_files():
+    # Create an empty list to store the JSON data
+    json_data = []
+
+    # Iterate over all JSON files in the specified directory
+    for file_path in DIRECTORY_PATH.rglob('*.json'):
+        # Read the JSON file
+        with open(file_path, 'r', encoding="utf-8") as file:
+            try:
+                # Load the JSON data
+                data = json.load(file)
+
+                # Append the data to the list
+                json_data.append(data)
+            except json.JSONDecodeError:
+                print(f"Error: Failed to parse JSON in file {file_path}")
+
+    # Create a dictionary with keys starting from 0
+    result_dict = {i: value for i, value in enumerate(json_data)}
+    with open(OUTPUT_PATH, 'w', encoding="utf-8") as file:
+        json.dump(result_dict, file, indent=4)
+
+
+# Print the resulting dictionary
+if __name__ == "__main__":
+    logging.info("Merging JSON files...")
+    print(merge_json_files())
+    logging.info("Done!")
