20c6df6f99c9cd2607f80ffa37a9e04b99bcc7c7
Jan Soukeník <59582869+Dzeniks@users.noreply.github.com>
2023-05-19 15:06:18 +0200

    Revert "Stable Diffusion (#10)"
    This reverts commit 387e5ae21569d04fb272b922b6104f79d880bb03.

--- backend/stable_diffusion.py
+++ backend/stable_diffusion.py
@@ -1,21 +1,15 @@
-import sys
-from pathlib import Path
-
+# example
 from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 import torch
-import PIL
 
-parent_folder = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(parent_folder))
+model_id = "stabilityai/stable-diffusion-2-1"
 
-import settings
+# Use the DPMSolverMultistepScheduler (DPM-Solver++) scheduler here instead
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe = pipe.to("cuda") # type: ignore
 
-class StableDiffusion:
-    def __init__(self) -> None:
-      self.pipeline = StableDiffusionPipeline.from_pretrained(settings.STABLE_DIFFUSION_MODEL_PATH, torch_dtype=torch.float16)
-      self.pipeline.scheduler = DPMSolverMultistepScheduler.from_config(self.pipeline.scheduler.config)
-      self.pipeline = self.pipeline.to("cuda")
-
-    def generate_image(self, prompt: str) -> PIL.Image.Image:
-      return self.pipeline(prompt).images[0]
+prompt = "Generate an image depicting an astronaut riding a majestic horse on the vast red landscapes of Mars. The astronaut should be dressed in a futuristic spacesuit, complete with a helmet and oxygen tank, while the horse should appear strong and agile, adapted to the alien environment. Let the image capture the sense of adventure and exploration as the astronaut and horse traverse the otherworldly terrain, with the backdrop showcasing the distinctive Martian features such as craters, mountains, and a reddish sky. Bring to life the unique juxtaposition of the technological and the natural, evoking a sense of wonder and curiosity about the possibilities that lie beyond our planet."
+image = pipe(prompt).images[0] # type: ignore
     
+image.save("astronaut_rides_horse.png")
